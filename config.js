f6b177f9da27012af10bab8c67f94b54de01d446
kianjuden-ship-it <kianjuden@gmail.com>
2026-07-19 08:04:37 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Edit API_BASE to point at your deployed backend before publishing this site.
-// Local development default assumes the backend from /backend running on port 5050.
-window.SUMAT_CONFIG = {
-  API_BASE: 'http://localhost:5050/api'
-};
